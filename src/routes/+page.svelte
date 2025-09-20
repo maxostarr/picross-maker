3fd8edbc14f19d4bda7a6d3fc66584ce69564495
Max Starr <max@maxstarr.xyz>
2025-09-19 22:30:13 -0500

    cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,9 +8,6 @@
 	let pixelArray: boolean[][] = $state([]);
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
 <ImageUpload bind:file />
 <ImageCanvas {imageSrc} bind:pixelArray />
 <Board {pixelArray} />
