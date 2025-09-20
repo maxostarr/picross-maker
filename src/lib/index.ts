147d9903f3ce6c3bf5da50d2b93a37e84a68d28f
Max Starr <max@maxstarr.xyz>
2025-09-20 18:25:01 -0500

    mild compression

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,10 +1,8 @@
-// place files you want to import through the `$lib` alias in this folder.
 
 export const queryifyBoard = ( board: number[][] ) => {
-  return board.map( row => row.join( ',' ) ).join( ';' );
+  return btoa( board.map( row => row.join( '' ) ).join( ';' ) )
 }
 
 export const dequeryifyBoard = ( str: string ) => {
-  console.log( str );
-  return str.split( ';' ).map( row => row.split( ',' ).map( cell => parseInt( cell ) ) );
+  return atob( str ).split( ';' ).map( row => row.split( '' ).map( cell => parseInt( cell ) ) );
 }
