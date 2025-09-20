47cafd3c4fe4f10dcfa2005aeb0e53f1c8c576f2
Max Starr <max@maxstarr.xyz>
2025-09-20 18:21:22 -0500

    cleanup

--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -6,19 +6,6 @@
 
 	let pixelArray: number[][] = dequeryifyBoard(data.board ?? '');
 	let showSolution = $state(false);
-
-	console.log(pixelArray);
-
-	// const urlParams = new URLSearchParams(window.location.search);
-
-	// $effect(() => {
-	// 	const w = urlParams.get('width');
-	// 	const h = urlParams.get('height');
-	// 	if (w) width = parseInt(w);
-	// 	if (h) height = parseInt(h);
-	// 	pixelArray = Array.from({ length: height }, () => Array.from({ length: width }, () => false));
-	// 	console.log('Initialized pixelArray with dimensions:', width, height);
-	// });
 </script>
 
 <Board {pixelArray} {showSolution} />
